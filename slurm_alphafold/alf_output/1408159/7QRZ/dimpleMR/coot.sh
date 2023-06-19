3e09b578473c4540551eb33e60fed22484b49cd5
Dominique Fastus <do8813fa-s@student.lu.se>
2023-06-19 18:16:01 +0200

    Add gitignore
    Former-commit-id: 159798e8a0fc6846a9f8f08b138d8cf13b6f5c20

--- slurm_alphafold/alf_output/1408159/7QRZ/dimpleMR/coot.sh
+++
@@ -1,1 +0,0 @@
-/sw/pkg/presto/software/CCP4/7.0.078-foss-2019b-SHELX-ARP-8.0-17/ccp4-7.0/bin/coot --no-guano /gpfs/offline1/staff/biomax/domfas/pipeline/alf_output/1408159/7QRZ/dimpleMR/blob1-coot.py
