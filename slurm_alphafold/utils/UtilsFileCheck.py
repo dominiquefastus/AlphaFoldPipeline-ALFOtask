51da3ff669864b7d7206423d91c2981b562fb165
Dominique Fastus <do8813fa-s@student.lu.se>
2023-11-01 15:34:10 +0100

    Comment and rename Utils functions

--- slurm_alphafold/utils/UtilsFileCheck.py
+++ slurm_alphafold/utils/UtilsFileCheck.py
@@ -1,24 +1,20 @@
 #!/usr/bin/env python3
 
+# import the necessary packages to log and interact with the file system
 import logging
 import os
-
-START_JOB_CODES = ["PENDING","RUNNING","REQUEUED", "RESIZING","SUSPENDED"]
-END_JOB_CODES = ["BOOT_FAIL","CANCELLED","COMPLETED","DEADLINE","FAILED","NODE_FAIL","OUT_OF_MEMORY","TIMEOUT","REVOKED","PREEMPTED"]
 
 logging.basicConfig(filename=f'job.log', filemode='w', format='%(asctime)s - %(message)s', level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
-# check if the output are complete and have the right format
-def check_out(self, out_dir, files_to_check):
+# function to check if the output are complete and have the right format
+# it simply checks if the files are present in the directory by looping over the files_to_check list
+def loop_files(output_dir, files_to_check):
 
     # Check if all files are present in the directory
     for file in files_to_check:
-        file_path = os.path.join(out_dir, file)
+        file_path = os.path.join(output_dir, file)
 
         if not os.path.isfile(file_path):
             logger.error(f"The files are not successfully generated, the {file} is missing...", exc_info=True)
             return False
-        
-    logger.info(f"The files are successfully generated in {out_dir}")
-    return True
--- slurm_alphafold/utils/UtilsMonitor.py
+++ slurm_alphafold/utils/UtilsMonitor.py
@@ -1,40 +1,62 @@
 #!/usr/bin/env python3
 
+# import the necessary packages to log, measure time and run subprocesses
 import subprocess
 import logging
 import time
 
+# define slurm job codes to check if the job is running or finished
 START_JOB_CODES = ["PENDING","RUNNING","REQUEUED", "RESIZING","SUSPENDED"]
-END_JOB_CODES = ["BOOT_FAIL","CANCELLED","COMPLETED","DEADLINE","FAILED","NODE_FAIL","OUT_OF_MEMORY","TIMEOUT","REVOKED","PREEMPTED"]
+# END_JOB_CODES = ["BOOT_FAIL","CANCELLED","COMPLETED","DEADLINE","FAILED","NODE_FAIL","OUT_OF_MEMORY","TIMEOUT","REVOKED","PREEMPTED"]
 
+# set up the logger
 logging.basicConfig(filename=f'job.log', filemode='w', format='%(asctime)s - %(message)s', level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 # make a continuos check if the job is finished on the server
+# the function takes as input the script to run and the name of the job
 def monitor_job(script, name):
+    # start the timer to measure the time needed to run the job
     start_time = time.perf_counter()
 
+    # submit the job to the server with the sbatch command
     job = subprocess.check_output(['sbatch', script])
+    # wait 2 seconds to let the job start
     time.sleep(2)
 
-    jobnum = (job.rstrip().split()[-1]).decode('ascii')
-    print(f"Starting Job: {jobnum} [{name}]")
+    # get the job id from the output of the sbatch command
+    # the job id is the last element of the output
+    # the output is a byte string, so we need to decode it to ascii
+    job_id = (job.rstrip().split()[-1]).decode('ascii')
+    # print the job id and the name of the job
+    print(f"Starting Job: {job_id} [{name}]")
 
+    # check the status of the job on the server and break the loop when the job is finished
+    # wait 1 second between each check
     while True:
-        print("{0}{1}{2}".format('waiting for ',name,' ...'),end='\r')
-        job = subprocess.check_output(['sacct','-j',jobnum]).decode('ascii')
+        # print a waiting phrase to let the user know that the job is running
+        print("waiting for {0}{1}".format(name,' ...'), end='\r')
+        # display accoutning information for the job and decode the output to ascii
+        job = subprocess.check_output(['sacct','-j',job_id]).decode('ascii')
+        # map the job status to over the job codes to check if the job is finished
+        # applies the lambda function to each element of the codes list and returns a list of booleans
+        # indicat the presence of the job code in the job status
         result = map(lambda x: x in job,START_JOB_CODES)
 
+        # if the job is finished, break the loop
         if not True in result:
             break
+        # wait 1 second between each check
         time.sleep(1)
 
-
+    # end the timer to measure the time needed to run the job
+    # calculate the total time needed to run the job
     end_time = time.perf_counter()
     total_time = end_time - start_time
 
     print(120*"=")
-    # logger.info(f"Job {job_id} {squeue_status}")
+    # log the job id and the name of the job and the time needed to run the job
     logger.info(f"Job {job} finished in {total_time} seconds")
 
-    return jobnum
+    # return the job id for further use
+    return job_id
